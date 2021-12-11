eb958e7e6d93301b3fbaf89cdf9da9064441769e
Aytacrustemli <87972434+AytacRustml@users.noreply.github.com>
2021-12-11 17:18:02 +0400

    user

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
-var b=document.querySelector("button");
-var h=document.querySelector("#col");
-var s=document.querySelector("#sol");
 
-b.onclick=function() {
-h.style.color="red"
-s.style.color="red"
-console.log("salam")
-}
+$("#accordion button").click(function () {
+    $("#accordion p").stop().slideUp()
+    $(this).next().stop().slideToggle()
+    
+})
